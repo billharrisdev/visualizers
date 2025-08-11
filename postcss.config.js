cc1114e0ae5a8354848c6a38edc03b5b9941b5ab
Bill <bill.harris.dev@gmail.com>
2025-08-10 20:18:08 -0700

    Trying another way to get tailwind working

---
+++ postcss.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  plugins: {
+    "@tailwindcss/postcss": {},
+    autoprefixer: {},
+  },
+};
+// This configuration file sets up PostCSS with Tailwind CSS and Autoprefixer.
+// It allows for the use of Tailwind's utility-first CSS framework and ensures that
+// CSS is compatible with a wide range of browsers by adding vendor prefixes where necessary.
